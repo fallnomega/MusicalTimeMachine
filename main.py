a5bc0158f471ec12e30048c6aff08078ea459723
fallnomega <fallnomega@gmail.com>
2023-03-17 16:47:42 -0700

    can now add to new playlist, cleaned up some testing code, and added user input option for the time to pull billboard list from.

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import spotify
 
 URL = 'https://www.billboard.com/charts/hot-100/'
 
-# time_period = input("Pull music from which year you want to list to; provide it in this format: YYYY-MM-DD:  ")
-time_period = '2000-08-12'
+time_period = input("Pull music from which year you want to list to; provide it in this format: YYYY-MM-DD:  ")
+# time_period = '2020-05-17'
+year = time_period.split('-')[0]
+
 requestor = requests.get(url=f"{URL}{time_period}/")
 requestor.raise_for_status()
 soup = BeautifulSoup(requestor.text, 'html.parser')
@@ -19,13 +21,16 @@ for x in the_hits_list:
     all_one_list.append({"rank": ranking, "artist": artist_name, "song": song_name})
 
 spot_client = spotify.Spotty()
+make_new_playlist = spot_client.create_playlists(time_period)
+playlist_id = make_new_playlist['id']
 
 spotify_uris = []
+
 for x in all_one_list:
-    spotify_uris.append(spot_client.search_track(x['artist'],x['song']))
-# spot_client.search_track('Sisqo','Incomplete')
+    if spot_client.search_track(x['artist'], x['song'], year) == None:
+        continue
+    else:
+        spotify_uris.append(spot_client.search_track(x['artist'], x['song'], year))
 print(spotify_uris)
 
-
-
-# spot_client.get_playlists()
+spot_client.add_track_to_playlist(track_uri=spotify_uris, pl_id=playlist_id)
--- spotify.py
+++ spotify.py
@@ -6,26 +6,28 @@ class Spotty:
     def __init__(self):
         self.client_id = os.environ.get('CLIENT_ID')
         self.client_secret = os.environ.get('CLIENT_SECRET')
-        self.scope = 'playlist-read-private'
+        self.scope = 'playlist-modify-private'
         self.auth_manager = spotipy.SpotifyOAuth(client_id=self.client_id, client_secret=self.client_secret,
                                                  redirect_uri='http://example.com', scope=self.scope)
         self.client = spotipy.Spotify(auth_manager=self.auth_manager)
 
-    def get_playlists(self):
-        # print(self.client.current_user())
-        # self.search_track()
-        return 0
+    def create_playlists(self, timestamp):
+        playlist_name = f"{timestamp} Billboard 100"
+        temp = self.client.current_user()
+        id = temp['id']
+        new_playlist = self.client.user_playlist_create(user=id, name=playlist_name, public=False, collaborative=False,
+                                                        description='Python learning task')
+        return new_playlist
 
-    def search_track(self,band,song):
-        # search_results = self.client.search(q="artist:Sisqo track:Incomplete", type="track")
+    def search_track(self, band, song, year):
+        # print(f"Search: {band} - {song}")
+        search_results = self.client.search(q=f"artist:{band} track:{song} year:{year}", type="track")
         try:
-            print(f"Search: {band} - {song}")
-            search_results = self.client.search(q=f"artist:{band} track:{song} year:2000", type="track")
             song_uri = search_results['tracks']['items'][0]['id']
+            return search_results['tracks']['items'][0]['id']
         except IndexError as error:
             print("*** No track found, skipping it ***")
-            pass
-        else:
-            print(search_results['tracks']['items'][0]['id'])
-            return search_results['tracks']['items'][0]['id']
 
+    def add_track_to_playlist(self, track_uri, pl_id):
+        self.client.playlist_add_items(playlist_id=pl_id, items=track_uri)
+        return 0
